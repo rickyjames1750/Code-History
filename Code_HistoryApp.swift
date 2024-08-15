df458bad531cccaeab874f02b742e56e0174214b
Ricky J. Sparks <rickyjames1750@gmail.com>
2024-08-14 20:11:23 -0700

    Initial commit of Code History project

---
+++ Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Ricky Sparks on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
